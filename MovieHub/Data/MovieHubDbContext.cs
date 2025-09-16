0e4a472cc80c7dc6b65d5637bb73218f32ecc840
Moynul Islam <moynul2015@cseku.ac.bd>
2025-09-16 18:19:53 +0600

    MH-3: Configure EF Core with SQL Server

---
+++ MovieHub/Data/MovieHubDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieHub.API.Data
+{
+    public class MovieHubDbContext : DbContext
+    {
+        public MovieHubDbContext(DbContextOptions<MovieHubDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- MovieHub/Program.cs
+++ MovieHub/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using MovieHub.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+#region Services
+// Register controllers
 builder.Services.AddControllers();
+
+// Register DbContext with SQL Server
+builder.Services.AddDbContext<MovieHubDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+#region Middlewares
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+#endregion
 
 app.MapControllers();
 
