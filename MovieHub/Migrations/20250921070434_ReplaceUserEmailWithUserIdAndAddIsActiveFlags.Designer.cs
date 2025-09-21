9268e63337013e9147fbb166045c429f6f31db25
Moynul Islam <moynul2015@cseku.ac.bd>
2025-09-21 13:30:07 +0600

    MH-4: Replaced UserEmail with UserId for user identification and added IsActive flag

--- MovieHub/Migrations/20250921070434_ReplaceUserEmailWithUserIdAndAddIsActiveFlags.Designer.cs
+++ MovieHub/Migrations/20250921070434_ReplaceUserEmailWithUserIdAndAddIsActiveFlags.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieHub.API.Data;
 
@@ -11,9 +12,11 @@ using MovieHub.API.Data;
 namespace MovieHub.API.Migrations
 {
     [DbContext(typeof(MovieHubDbContext))]
-    partial class MovieHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921070434_ReplaceUserEmailWithUserIdAndAddIsActiveFlags")]
+    partial class ReplaceUserEmailWithUserIdAndAddIsActiveFlags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
