30bf66b577d9c025219f17e7880699d07ead330a
Moynul Islam <moynul2015@cseku.ac.bd>
2025-11-04 14:07:42 +0600

    MH-21: Fix the data annotation and error messages in the MovieCreateDto.

--- MovieHub.API/DTOs/Base/MovieDtoBase.cs
+++ MovieHub.API/DTOs/Base/MovieDtoBase.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using MovieHub.API.Constants;
 
 namespace MovieHub.API.DTOs.Base
@@ -19,7 +20,7 @@ namespace MovieHub.API.DTOs.Base
         public string Title
         {
             get => _title;
-            set => _title = value?.Trim() ?? String.Empty;
+            set => _title = Regex.Replace(value.Trim(), @"\s+", " ");
         }
 
         private string _genre = String.Empty;
@@ -36,7 +37,7 @@ namespace MovieHub.API.DTOs.Base
         public string Genre
         {
             get => _genre;
-            set => _genre = value?.Trim() ?? String.Empty;
+            set => _genre = Regex.Replace(value.Trim(), @"\s+", " ");
         }
 
         [Required(ErrorMessage = ErrorMessages.Movie.DurationRequired)]
@@ -61,33 +62,43 @@ namespace MovieHub.API.DTOs.Base
         public string Director
         {
             get => _director;
-            set => _director = value?.Trim() ?? String.Empty;
+            set => _director = Regex.Replace(value.Trim(), @"\s+", " ");
         }
 
-        [MinLength(ValidationConstants.Movie.MinDescriptionLength)]
-        [MaxLength(ValidationConstants.Movie.MaxDescriptionLength)]
-        public string Description { get; set; } = string.Empty;
+        private string _description = String.Empty;
+        [MinLength(ValidationConstants.Movie.MinDescriptionLength, ErrorMessage = ErrorMessages.Movie.DescriptionMinLengthRequired)]
+        [MaxLength(ValidationConstants.Movie.MaxDescriptionLength, ErrorMessage = ErrorMessages.Movie.DescriptionMaxLengthExceeded)]
+        public string Description
+        {
+            get => _description;
+            set => _description = Regex.Replace(value.Trim(), @"\s+", " ");
+        }
 
+        private string _posterUrl = String.Empty;
         [Required(ErrorMessage = ErrorMessages.Movie.PosterUrlRequired)]
         [MaxLength(
             ValidationConstants.Movie.MaxPosterUrlLength,
             ErrorMessage = ErrorMessages.Movie.PosterUrlMaxLengthExceeded
         )]
         [Url(ErrorMessage = ErrorMessages.Movie.InvalidPosterUrlFormat)]
-        public string PosterUrl { get; set; } = string.Empty;
+        public string PosterUrl
+        {
+            get => _posterUrl;
+            set => _posterUrl = value.Trim();
+        }
 
         private string _actors = String.Empty;
 
-        [Required(ErrorMessage = ErrorMessages.Movie.InvalidActorsFormat)]
-        [MaxLength(ValidationConstants.Movie.MaxActorsLength)]
+        [Required(ErrorMessage = ErrorMessages.Movie.ActorsRequired)]
+        [MaxLength(ValidationConstants.Movie.MaxActorsLength, ErrorMessage = ErrorMessages.Movie.ActorsMaxLengthExceeded)]
         [RegularExpression(
-            ValidationConstants.NameRegex,
-            ErrorMessage = ErrorMessages.InvalidNameFormat
+            ValidationConstants.Movie.ActorsRegex,
+            ErrorMessage = ErrorMessages.Movie.InvalidActorsFormat
         )]
         public string Actors
         {
             get => _actors;
-            set => _actors = value?.Trim() ?? String.Empty;
+            set => _actors = Regex.Replace(value.Trim(), @"\s+", " ");
         }
     }
 }
