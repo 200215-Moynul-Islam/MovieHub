15fce02e62249c600b2e4118096c4b87a80318fd
Moynul Islam <moynul2015@cseku.ac.bd>
2025-11-13 19:04:26 +0600

    MH-36: Implement the service layer.

--- MovieHub.API/Services/IMovieHallShowTimeService.cs
+++ MovieHub.API/Services/IMovieHallShowTimeService.cs
@@ -5,5 +5,9 @@ namespace MovieHub.API.Services
     public interface IMovieHallShowTimeService
     {
         Task<int> CreateShowTimeAsync(ShowTimeCreateDto showTimeCreateDto);
+        Task UpdateShowTimeByIdAsync(
+            int id,
+            ShowTimeUpdateDto showTimeUpdateDto
+        );
     }
 }
--- MovieHub.API/Services/MovieHallShowTimeService.cs
+++ MovieHub.API/Services/MovieHallShowTimeService.cs
@@ -49,6 +49,51 @@ namespace MovieHub.API.Services
             return showTime.Id;
         }
 
+        public async Task UpdateShowTimeByIdAsync(
+            int showTimeId,
+            ShowTimeUpdateDto showTimeUpdateDto
+        )
+        {
+            var showTime = await GetShowTimeByIdOrThrowAsync(showTimeId);
+            await EnsureShowTimeHasNotStartedOrThrowAsync(showTime);
+
+            // Get the movie duration using the updated MovieId, or use the existing one if MovieId is not provided in the ShowTimeUpdateDto.
+            var movieDuration = await GetMovieDurationByMovieIdOrThrowAsync(
+                showTimeUpdateDto.MovieId is null
+                    ? showTime.MovieId!.Value
+                    : showTimeUpdateDto.MovieId.Value
+            );
+
+            // Use the existing HallId . If an updated HallId is provided, verify it exists before using it.
+            var hallId = showTime.HallId!.Value;
+            if (showTimeUpdateDto.HallId is not null)
+            {
+                await EnsureHallExistByIdOrThrowAsync(
+                    showTimeUpdateDto.HallId.Value
+                );
+                hallId = showTimeUpdateDto.HallId.Value;
+            }
+
+            // Use the updated StartTime if provided; otherwise, keep the existing one.
+            var startTime = showTimeUpdateDto.StartTime is not null
+                ? showTimeUpdateDto.StartTime.Value
+                : showTime.StartTime!.Value;
+            // Use the updated BufferMinutes if provided; otherwise, keep the existing one.
+            var bufferMinutes = showTimeUpdateDto.BufferMinutes is not null
+                ? showTimeUpdateDto.BufferMinutes.Value
+                : showTime.BufferMinutes!.Value;
+            await EnsureNoConflictingShowTimeExistsInHallOrThrowAsync(
+                startTime,
+                startTime.AddMinutes(movieDuration + bufferMinutes),
+                hallId,
+                showTimeId
+            );
+
+            _mapper.Map(showTimeUpdateDto, showTime);
+            await _showTimeRepository.SaveChangesAsync();
+            return;
+        }
+
         #region Private Methods
         private async Task<int> GetMovieDurationByMovieIdOrThrowAsync(
             int movieId
@@ -90,6 +135,50 @@ namespace MovieHub.API.Services
                 throw new Exception(
                     "Cannot schedule this show. It overlaps with another show in the same hall."
                 );
+            }
+        }
+
+        private async Task EnsureNoConflictingShowTimeExistsInHallOrThrowAsync(
+            DateTime startTime,
+            DateTime endTime,
+            int hallId,
+            int currentShowTimeId
+        )
+        {
+            if (
+                await _showTimeRepository.HasAnyConflictingShowTimeInHallAsync(
+                    startTime,
+                    endTime,
+                    hallId,
+                    currentShowTimeId
+                )
+            )
+            {
+                throw new Exception(
+                    "Cannot schedule this show. It overlaps with another show in the same hall."
+                );
+            }
+        }
+
+        private async Task<ShowTime> GetShowTimeByIdOrThrowAsync(int showTimeId)
+        {
+            var showTime = await _showTimeRepository.GetByIdAsync(showTimeId);
+            if (showTime is null)
+            {
+                throw new Exception(
+                    $"ShowTime with id '{showTimeId}' does not exist."
+                );
+            }
+            return showTime;
+        }
+
+        private async Task EnsureShowTimeHasNotStartedOrThrowAsync(
+            ShowTime showTime
+        )
+        {
+            if (showTime.StartTime <= DateTime.UtcNow)
+            {
+                throw new Exception("This ShowTime has already started.");
             }
         }
         #endregion
