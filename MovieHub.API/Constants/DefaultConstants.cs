71236aa8d0de914c2062c2a25cf96443caf63d31
Moynul Islam <87566898+200215-Moynul-Islam@users.noreply.github.com>
2025-11-25 21:19:53 +0600

    MH-42: Add default system roles
    * MH-42: Add default system roles.

--- MovieHub.API/Constants/DefaultConstants.cs
+++ MovieHub.API/Constants/DefaultConstants.cs
@@ -4,5 +4,16 @@ namespace MovieHub.API.Constants
     {
         public const int Offset = 0;
         public const int Limit = 20;
+
+        public static class Role
+        {
+            public const int AdminRoleId = 1;
+            public const int MangerRoleId = 2;
+            public const int UserRoleId = 3;
+
+            public const string AdminRoleName = "Admin";
+            public const string MangerRoleName = "Manager";
+            public const string UserRoleName = "User";
+        }
     }
 }
